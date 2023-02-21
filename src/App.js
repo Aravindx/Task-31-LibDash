0f6652bc5614f5042fc5067769685ccaa3839f45
Aravindx <aravindmathi517@gmail.com>
2023-02-21 15:50:54 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-
-import './App.css';
-import { Dashboard } from './Dashboard';
-import { Navbar } from './Navbar';
-import { Sidebar } from './Sidebar';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import { Books } from './Books';
-import { Addbook } from './Addbook';
-import ViewBook from './ViewBook';
-import { Editbook } from './Editbook';
+import "./App.css";
+import { Dashboard } from "./Dashboard";
+import { Navbar } from "./Navbar";
+import { Sidebar } from "./Sidebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Books } from "./Books";
+import { Addbook } from "./Addbook";
+import ViewBook from "./ViewBook";
+import { Editbook } from "./Editbook";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <div className='row'>
+        <div className="row">
           <Sidebar />
-          <div className='col-lg-10 half ' >
+          <div className="col-lg-10 half ">
             <Navbar />
             <Routes>
               <Route path="/dashboard" element={<Dashboard />}></Route>
@@ -28,15 +23,11 @@ function App() {
               <Route path="/viewbook/:id" element={<ViewBook />}></Route>
               <Route path="/editbook/:id" element={<Editbook />}></Route>
             </Routes>
-
           </div>
         </div>
       </div>
     </BrowserRouter>
   );
-
-
 }
 
 export default App;
-
